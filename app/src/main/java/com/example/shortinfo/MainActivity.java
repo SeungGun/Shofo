b94e7643899e5fc71997aab25a8c7631b6f2c8de
SeungGun <saverrw2864@naver.com>
2021-07-03 14:15:02 +0900

    newly modified vaccine part with modifying domestic part explanation and standard time of world collection

--- app/src/main/java/com/example/shortinfo/MainActivity.java
+++ app/src/main/java/com/example/shortinfo/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView vaccineFirstText;
     private TextView distancingText;
     private TextView vaccineSecondText;
+    private TextView worldStdTime;
     private ArrayList<String> distanceList;
     private static final int defaultRegionNumber = 8; //경기도
     private TextView worldConfirmedText;
@@ -73,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         distancingText = findViewById(R.id.corona_text_distancing);
         worldConfirmedText = findViewById(R.id.corona_text_world);
         worldConfirmedVarText = findViewById(R.id.corona_text_world_var);
-
+        worldStdTime = findViewById(R.id.corona_text_world_std_time);
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -118,7 +119,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Document vaccineUrl = null;
+                try {
+                    vaccineUrl = Jsoup.connect("https://search.naver.com/search.naver?sm=tab_sug.top&where=nexearch&query=%EC%BD%94%EB%A1%9C%EB%82%98+%EB%B0%B1%EC%8B%A0+%EC%A0%91%EC%A2%85+%ED%98%84%ED%99%A9&oquery=%EC%BD%94%EB%A1%9C%EB%82%98&tqi=hLI4RlprvxsssdKQRURssssss4C-138619&acq=%EC%BD%94%EB%A1%9C%EB%82%98+%EB%B0%B1&acr=4&qdt=0").get();
+                    Elements elements;
+                    elements = vaccineUrl.select("div.vaccine_status_item");
+                    boolean flag = false;
+                    for(Element e : elements){
+                        if(!flag) {
+                            vaccineFirst = e.text().split(" ");
+                            flag = true;
+                        }
+                        else{
+                            vaccineSecond = e.text().split(" ");
+                            break;
+                        }
+                    }
+                    //[0] ~ [2] : 전국 n차 접종
+                    //[3] : n%
+                    //[4] : 누적n + 명
+                    //[5] : 신규n증가 - 증가 + 명
+                    String firstVacc = processVaccineFirst(vaccineFirst);
+                    String secondVacc = processVaccineFirst(vaccineSecond);
 
+                    bundle.putString("domestic_vaccine_first", firstVacc);
+                    bundle.putString("domestic_vaccine_second",secondVacc);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
         new Thread() {
             @Override
             public void run() {
@@ -168,28 +204,15 @@ public class MainActivity extends AppCompatActivity {
                     Elements elements = doc.select("div.csp_infoCheck_area._togglor_root a.info_text._trigger");
                     bundle.putString("today_std_time", elements.select("span._update_time").text());
 
-                    /*elements = doc.select("div.vaccine_status_item_inner");
+                    elements = doc.select("div.patients_info div.csp_infoCheck_area._togglor_root a.info_text._trigger");
+                    int id = elements.text().indexOf("세계현황");
+                    String s1 = elements.text().substring(id);
+                    int id2 = s1.substring(4).indexOf("세계현황");
 
-                    boolean flag = false;
-                    for(Element e : elements){
-                        if(!flag) {
-                            vaccineFirst = e.text().split(" ");
-                            flag = true;
-                        }
-                        else{
-                            vaccineSecond = e.text().split(" ");
-                        }
-                    }
-                    //[0] ~ [2] : 전국 n차 접종
-                    //[3] : n%
-                    //[4] : 누적n + 명
-                    //[5] : 신규n증가 - 증가 + 명
-                    String firstVacc = processVaccineFirst(vaccineFirst);
-                    String secondVacc = processVaccineFirst(vaccineSecond);
+                    String sub = s1.substring(4 + id2);
+                    Log.d("world_std_time", sub);
 
-                    bundle.putString("domestic_vaccine_first", firstVacc);
-                    bundle.putString("domestic_vaccine_second",secondVacc);
-                    */
+                    bundle.putString("world_std_time",sub);
                     Message msg = handler.obtainMessage();
                     msg.setData(bundle);
                     handler.sendMessage(msg);
@@ -212,19 +235,20 @@ public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(Message msg) {
-            confirmedText.setText("확진자 → " + msg.getData().getString("confirmed"));
+            confirmedText.setText("국내 확진자 → " + msg.getData().getString("confirmed"));
             confirmedVarText.setText( " ▲ " + msg.getData().getString("confirmed_var"));
             confirmedDetailText.setText("(국내: " + msg.getData().getString("today_domestic") + " , 해외: " + msg.getData().getString("today_abroad") + ")");
-            releaseText.setText("격리해제 → " + msg.getData().getString("release"));
+            releaseText.setText("국내 격리해제 → " + msg.getData().getString("release"));
             releaseVarText.setText(" ▲ " + msg.getData().getString("release_var"));
-            deadText.setText("사망자 → " + msg.getData().getString("dead"));
+            deadText.setText("국내 사망자 → " + msg.getData().getString("dead"));
             deadVarText.setText( " ▲ " + msg.getData().getString("dead_var"));
-            stdDateText.setText("※ 집계 기준 시간 " + msg.getData().getString("today_std_time"));
-//            vaccineFirstText.setText(msg.getData().getString("domestic_vaccine_first"));
-//            vaccineSecondText.setText(msg.getData().getString("domestic_vaccine_second"));
+            stdDateText.setText("※ 국내 집계 기준 시간 " + msg.getData().getString("today_std_time"));
+            vaccineFirstText.setText(msg.getData().getString("domestic_vaccine_first"));
+            vaccineSecondText.setText(msg.getData().getString("domestic_vaccine_second"));
             distancingText.setText("※ 거리두기 " +distanceList.get(defaultRegionNumber));
             worldConfirmedText.setText(" → "+msg.getData().getString("world"));
             worldConfirmedVarText.setText(" ▲ "+msg.getData().getString("world_var"));
+            worldStdTime.setText("※ "+msg.getData().getString("world_std_time"));
         }
     };
 
